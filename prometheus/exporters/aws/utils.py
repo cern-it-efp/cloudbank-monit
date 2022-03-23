72498afe39931d25a7e7264a43ff0a463e189730
Apostolos <apostolos.theodoridis@cern.ch>
2022-03-23 11:39:57 +0100

    AWS extractor update

--- prometheus/exporters/aws/utils.py
+++ prometheus/exporters/aws/utils.py
@@ -19,40 +19,6 @@ import pycountry
 local_path = "C:/Users/atheodor/AppData/Local/Temp"
 local_path = "/tmp"
 
-# def aws_region_to_country(region_from_csv):
-#     # TODO - complete with https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html
-#     all_aws_regions = {
-#         "us-east-2": "US",
-#         "us-east-1": "US",
-#         "us-west-1": "US",
-#         "us-west-2": "US",
-#         "af-south-1": "ZA",
-#         "ap-east-1": "HK",
-#         "ap-southeast-3	": "ID",
-#         "ap-south-1": "IN",
-#         "ap-northeast-3": "JP",
-#         "ap-northeast-2": "KR",
-#         "ap-southeast-1": "SG",
-#         "ap-southeast-2": "AU",
-#         "ap-northeast-1": "JP",
-#         "ca-central-1": "CA",
-#         "eu-central-1": "DE",
-#         "eu-west-1": "IR",
-#         "eu-west-2": "GB",
-#         "eu-south-1": "IT",
-#         "eu-west-3": "FR",
-#         "eu-north-1": "SE",
-#         "me-south-1": "BH",
-#         "sa-east-1": "BR",
-#         "us-gov-east-1": "US",
-#         "us-gov-west-1": "US"
-#     }
-#     try:
-#         return all_aws_regions[region_from_csv]
-#     except:
-#         return "Error"
-#print(aws_region_to_country("sa-east-1"))
-
 def getGroups():
     """ Returns a dictionary containing properties, groupdID:name. """
 
@@ -146,17 +112,11 @@ def getAWS():
     #df[['region','city']]= df.productlocation.str.split('\(|\)', expand=True).iloc[:,[0,1]]
     #df[['continent','direction']]= df.region.str.split(' ', expand=True).iloc[:,[0,1]]
 
-    ###### Filter data: take only the consumption from the last 24 hours
-
-
-    #df[['country']] = df.productregion.aws_region_to_country("productregion")
     #df.loc[df['productregion'] == 'us-east-2', 'productregion'] = 'US'
-    #df
+
     df['product/region'] = df['product/region'].replace(['us-east-2','us-east-1','us-west-1','us-west-2','af-south-1','ap-east-1','ap-southeast-3','ap-south-1','ap-northeast-3','ap-northeast-2','ap-southeast-1','ap-southeast-2','ap-northeast-1','ca-central-1','eu-central-1','eu-west-1','eu-west-2','eu-south-1','eu-west-3','eu-north-1','me-south-1','sa-east-1','us-gov-east-1','us-gov-west-1'],
                                                       ['US','US','US','US','ZA','HK','ID','IN','JP','KR','SG','AU','JP','CA','DE','IR','GB','IT','FR','SE','BH','BR','US','US'])
-
-    print(df)
-    #df[['countries']] = mapper
+    ###### Filter data: take only the consumption from the last 24 hours
     period = 48
     df["lineItem/UsageStartDate"] = pd.to_datetime(df["lineItem/UsageStartDate"]).dt.tz_localize(None)
     startTime = (dt.datetime.now()-dt.timedelta(hours=period))
